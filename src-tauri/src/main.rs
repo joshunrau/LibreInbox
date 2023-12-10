d9796de820ba2475a1616450555ce4f1efd67651
joshunrau <contact@joshuaunrau.com>
2023-12-10 18:24:53 -0500

    add code from reading book

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,11 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+pub mod basics;
+pub mod collections;
+pub mod enums;
+pub mod structs;
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
