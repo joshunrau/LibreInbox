5530139d27d47281adf7ce862f4043506d6ca5b5
joshunrau <contact@joshuaunrau.com>
2023-12-10 13:52:54 -0500

    add alias

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,20 @@
+import path from 'path';
+import url from 'url';
+
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   clearScreen: false,
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src')
+    }
+  },
   server: {
     port: 1420,
     strictPort: true
