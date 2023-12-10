48c034aba505d41b92fc6c9de4edb660f886aef8
joshunrau <contact@joshuaunrau.com>
2023-12-10 13:18:29 -0500

    add eslint and prettier

---
+++ eslint.config.js
@@ -0,0 +1,3 @@
+import { createConfig } from '@joshunrau/eslint-config';
+
+export default createConfig();
---
+++ prettier.config.js
@@ -0,0 +1,8 @@
+/** @type {import("prettier").Options} */
+export default {
+  htmlWhitespaceSensitivity: 'ignore',
+  plugins: ['prettier-plugin-tailwindcss'],
+  printWidth: 120,
+  singleQuote: true,
+  trailingComma: 'none',
+};
