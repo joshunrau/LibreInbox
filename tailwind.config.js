2536c56852c3786494b9bdf023161c5456a7e8b8
joshunrau <contact@joshuaunrau.com>
2023-12-10 14:35:25 -0500

    add dm

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ import formsPlugin from '@tailwindcss/forms';
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  darkMode: ['class', '[data-mode="dark"]'],
   plugins: [formsPlugin],
   theme: {
     extend: {}
