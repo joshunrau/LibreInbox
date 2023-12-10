2420411abd11d121e1aee05b8f78cd7c4de78107
joshunrau <contact@joshuaunrau.com>
2023-12-10 14:17:54 -0500

    add storybook

---
+++ src/components/Layout.stories.tsx
@@ -0,0 +1,9 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { Layout } from './Layout';
+
+type Story = StoryObj<typeof Layout>;
+
+export default { component: Layout } as Meta<typeof Layout>;
+
+export const Default: Story = {};
