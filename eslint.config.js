f0694c79039a07b50f4b10ede897a834c6481d2a
joshunrau <contact@joshuaunrau.com>
2023-12-10 13:22:01 -0500

    remove bloat

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,17 @@
+import path from 'path';
+import url from 'url';
+
 import { createConfig } from '@joshunrau/eslint-config';
 
-export default createConfig();
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default createConfig({
+  base: {
+    env: 'browser'
+  },
+  jsx: true,
+  ts: {
+    project: path.resolve(__dirname, 'tsconfig.json')
+  }
+});
--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import { invoke } from "@tauri-apps/api/tauri";
-import "./App.css";
+import { useState } from 'react';
 
-function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
+import { invoke } from '@tauri-apps/api/tauri';
+
+export const App = () => {
+  const [greetMsg, setGreetMsg] = useState('');
+  const [name, setName] = useState('');
 
   async function greet() {
     // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    setGreetMsg(await invoke("greet", { name }));
+    setGreetMsg(await invoke('greet', { name }));
   }
 
   return (
@@ -17,14 +16,11 @@ function App() {
       <h1>Welcome to Tauri!</h1>
 
       <div className="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
+        <a href="https://vitejs.dev" rel="noreferrer" target="_blank">
+          <img alt="Vite logo" className="logo vite" src="/vite.svg" />
         </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href="https://tauri.app" rel="noreferrer" target="_blank">
+          <img alt="Tauri logo" className="logo tauri" src="/tauri.svg" />
         </a>
       </div>
 
@@ -34,20 +30,14 @@ function App() {
         className="row"
         onSubmit={(e) => {
           e.preventDefault();
-          greet();
+          void greet();
         }}
       >
-        <input
-          id="greet-input"
-          onChange={(e) => setName(e.currentTarget.value)}
-          placeholder="Enter a name..."
-        />
+        <input id="greet-input" placeholder="Enter a name..." onChange={(e) => setName(e.currentTarget.value)} />
         <button type="submit">Greet</button>
       </form>
 
       <p>{greetMsg}</p>
     </div>
   );
-}
-
-export default App;
+};
