d29f2b0f6ab201acbb6ce469c744cdd4713e4329
joshunrau <contact@joshuaunrau.com>
2024-03-09 16:02:24 -0500

    add date picker

--- src/components/base/ArrowToggle/ArrowToggle.tsx
+++ src/components/base/ArrowToggle/ArrowToggle.tsx
@@ -3,18 +3,20 @@ import React, { useMemo } from 'react';
 import { ChevronUpIcon } from '@radix-ui/react-icons';
 import type { Simplify } from 'type-fest';
 
+import { cn } from '@/utils/cn';
+
 import { Button, type ButtonProps } from '../Button';
 
 export type ArrowToggleProps = Simplify<
   React.HTMLAttributes<HTMLButtonElement> & {
     /** Whether or not the arrow is currently toggled */
-    isToggled: boolean;
+    isToggled?: boolean;
 
     /** The starting position of the arrow (i.e., which direction does it point to) */
     position: 'down' | 'left' | 'right' | 'up';
 
     /** The clockwise rotation of the arrow when toggled (e.g., if the position is 'right' and rotation is 90, the arrow will point down) */
-    rotation: number;
+    rotation?: number;
 
     /** The variant of button to use */
     variant?: Extract<ButtonProps['variant'], 'ghost' | 'outline'>;
@@ -22,7 +24,7 @@ export type ArrowToggleProps = Simplify<
 >;
 
 export const ArrowToggle = React.forwardRef<HTMLButtonElement, ArrowToggleProps>(function ArrowToggle(
-  { className, isToggled, position, rotation, variant = 'ghost', ...props },
+  { className, isToggled, position, rotation = 0, variant = 'ghost', ...props },
   ref
 ) {
   const computedRotation = useMemo(() => {
@@ -40,7 +42,7 @@ export const ArrowToggle = React.forwardRef<HTMLButtonElement, ArrowToggleProps>
   }, [position, rotation, isToggled]);
 
   return (
-    <Button className={className} ref={ref} size="icon" type="button" variant={variant} {...props}>
+    <Button className={cn('h-6 w-6', className)} ref={ref} size="icon" type="button" variant={variant} {...props}>
       <ChevronUpIcon
         className="transform-gpu transition-transform"
         data-testid="arrow-up-icon"
