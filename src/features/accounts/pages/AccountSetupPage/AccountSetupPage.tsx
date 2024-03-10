57e1a4b90ace8ea86b7e3c8f2dcd6994f80cf784
joshunrau <contact@joshuaunrau.com>
2024-03-10 15:51:04 -0400

    add translations

--- src/features/accounts/pages/AccountSetupPage/AccountSetupPage.tsx
+++ src/features/accounts/pages/AccountSetupPage/AccountSetupPage.tsx
@@ -1,5 +1,7 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 
+import { useTranslation } from 'react-i18next';
+
 import icon from '@/assets/icon.png';
 import { Button } from '@/components/Button';
 import { Heading } from '@/components/Heading';
@@ -7,14 +9,15 @@ import { LanguageToggle } from '@/components/LanguageToggle';
 import { ThemeToggle } from '@/components/ThemeToggle';
 
 export const AccountSetupPage = () => {
+  const { t } = useTranslation();
   return (
     <div className="grid h-full bg-muted">
       <div className="flex flex-col items-center justify-center gap-3">
         <img alt="LibreInbox Icon" className="h-24 w-24" src={icon} />
         <Heading className="mb-3" variant="h1">
-          Welcome to LibreInbox
+          {t('greeting')}
         </Heading>
-        <Button className="w-full max-w-64">Add Account</Button>
+        <Button className="w-full max-w-64">{t('addAccount')}</Button>
       </div>
       <div className="absolute bottom-2 right-2 flex gap-1">
         <ThemeToggle align="start" />
