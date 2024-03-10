f9ab6be8aa1a83e9871cf3015b4259ce25c84f96
joshunrau <contact@joshuaunrau.com>
2024-03-10 15:47:01 -0400

    adjust setup page

--- src/features/accounts/pages/AccountSetupPage/AccountSetupPage.tsx
+++ src/features/accounts/pages/AccountSetupPage/AccountSetupPage.tsx
@@ -1,62 +1,24 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 
-import { buttonVariants } from '@/components/Button';
-import { cn } from '@/utils/cn';
-
-import { AccountSetupForm } from '../../components/AccountSetupForm';
+import icon from '@/assets/icon.png';
+import { Button } from '@/components/Button';
+import { Heading } from '@/components/Heading';
+import { LanguageToggle } from '@/components/LanguageToggle';
+import { ThemeToggle } from '@/components/ThemeToggle';
 
 export const AccountSetupPage = () => {
   return (
-    <div className="container relative hidden h-[800px] flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
-      <a className={cn(buttonVariants({ variant: 'ghost' }), 'absolute right-4 top-4 md:right-8 md:top-8')} href="#">
-        Login
-      </a>
-      <div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
-        <div className="absolute inset-0 bg-zinc-900" />
-        <div className="relative z-20 flex items-center text-lg font-medium">
-          <svg
-            className="mr-2 h-6 w-6"
-            fill="none"
-            stroke="currentColor"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path d="M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3" />
-          </svg>
-          Acme Inc
-        </div>
-        <div className="relative z-20 mt-auto">
-          <blockquote className="space-y-2">
-            <p className="text-lg">
-              &ldquo;This library has saved me countless hours of work and helped me deliver stunning designs to my
-              clients faster than ever before.&rdquo;
-            </p>
-            <footer className="text-sm">Sofia Davis</footer>
-          </blockquote>
-        </div>
+    <div className="grid h-full bg-muted">
+      <div className="flex flex-col items-center justify-center gap-3">
+        <img alt="LibreInbox Icon" className="h-24 w-24" src={icon} />
+        <Heading className="mb-3" variant="h1">
+          Welcome to LibreInbox
+        </Heading>
+        <Button className="w-full max-w-64">Add Account</Button>
       </div>
-      <div className="lg:p-8">
-        <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
-          <div className="flex flex-col space-y-2 text-center">
-            <h1 className="text-2xl font-semibold tracking-tight">Create an account</h1>
-            <p className="text-sm text-muted-foreground">Enter your email below to create your account</p>
-          </div>
-          <AccountSetupForm />
-          <p className="px-8 text-center text-sm text-muted-foreground">
-            By clicking continue, you agree to our{' '}
-            <a className="underline underline-offset-4 hover:text-primary" href="/terms">
-              Terms of Service
-            </a>{' '}
-            and{' '}
-            <a className="underline underline-offset-4 hover:text-primary" href="/privacy">
-              Privacy Policy
-            </a>
-            .
-          </p>
-        </div>
+      <div className="absolute bottom-2 right-2 flex gap-1">
+        <ThemeToggle align="start" />
+        <LanguageToggle align="start" />
       </div>
     </div>
   );
